2274596a3f7308d95cbee4a102df07c3e4612c63
Roeh Lewit <69601986+roehlewit@users.noreply.github.com>
2020-10-19 18:51:30 -0400

    Responsive changes

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ const menu = document.querySelector('.toggle');
 // EL for nav menu
 menuBtn.addEventListener('click', () => {
 	if (menu.style.display === 'none') {
-        menu.style.display = 'flex';
+		menu.style.display = 'flex',
+		menu.style.height = 0 + '30px';
 	} else {
         menu.style.display = 'none';
 	}
