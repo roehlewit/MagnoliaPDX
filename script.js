e0df625cef8f0aab286fd89e871184b6ebb34a4a
Roeh Lewit <69601986+roehlewit@users.noreply.github.com>
2020-10-20 00:32:48 -0400

    resp adj

--- script.js
+++ script.js
@@ -2,6 +2,12 @@
 const menuBtn = document.getElementById('menu-btn');
 const menu = document.getElementById('toggle');
 
+//Form variables
+const form = document.getElementById('submit');
+const name = document.getElementById('fullName');
+const email = document.getElementById('email');
+const phone = document.getElementById('phone');
+
 // EL for nav menu
 menuBtn.addEventListener('click', () => {
 	if (menu.style.opacity === '0') {
@@ -12,4 +18,5 @@ menuBtn.addEventListener('click', () => {
 });
 
 
-AOS.init({duration: 500,});
+
+AOS.init({ duration: 500 });
