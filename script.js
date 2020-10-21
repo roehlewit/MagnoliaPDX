fb0960a503e582aaffa26e578f48075ba47564e3
Roeh Lewit <69601986+roehlewit@users.noreply.github.com>
2020-10-21 06:57:35 -0400

    click event managed, minify css

--- script.js
+++ script.js
@@ -1,22 +1,14 @@
 // UI Variables
 const menuBtn = document.getElementById('menu-btn');
 const menu = document.getElementById('toggle');
-
-//Form variables
-const form = document.getElementById('submit');
-const name = document.getElementById('fullName');
-const email = document.getElementById('email');
-const phone = document.getElementById('phone');
-
 // EL for nav menu
-menuBtn.addEventListener('click', () => {
-	if (menu.style.opacity === '0') {
+menuBtn.addEventListener('click', toggle);
+
+function toggle() {
+	if(menu.style.opacity == '0' || menu.style.opacity == ''){
 		menu.style.opacity = '1';
 	} else {
 		menu.style.opacity = '0';
 	}
-});
+}
 
-
-
-AOS.init({ duration: 500 });
